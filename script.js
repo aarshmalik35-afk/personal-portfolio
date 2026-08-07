b3ae25ae97f62b4bdb0bda4c9d029ed7f21e9ca8
Aarsh Malik <aarshmalik35@gmail.com>
2026-08-07 12:19:28 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function scrollToSection(id){
-document.getElementById(id).scrollIntoView({behavior:'smooth'});
-}
-
-document.getElementById("contactForm").addEventListener("submit", function(e){
-e.preventDefault();
-alert("Message sent successfully!");
-});
