b26cc63d9cf0d393390da00ea7abb08d4725e738
Aarsh Malik <aarshmalik35@gmail.com>
2026-08-07 12:18:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+function scrollToSection(id){
+document.getElementById(id).scrollIntoView({behavior:'smooth'});
+}
 
+document.getElementById("contactForm").addEventListener("submit", function(e){
+e.preventDefault();
+alert("Message sent successfully!");
+});
